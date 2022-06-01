b08625fc6cd8e2f4657d137c181c5a2e1fc9f795
MU-ZJ <murphy.zachary@outlook.com>
2022-05-31 20:36:23 -0400

    Attempted modal with new module.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+/* import { Button, Modal, Content } from 'react-bulma-components'; */
 
 function Nav() {
   return (
@@ -9,6 +10,53 @@ function Nav() {
         <NavLink to='/gallery' className="navbar-item">Gallery</NavLink>
       </div>
 
+{/*
+      <Button.Group renderAs={function noRefCheck(){}}>
+    <Button
+      color="info"
+      onClick={function noRefCheck(){}}
+    >
+      Open Card Modal
+    </Button>
+      </Button.Group>
+  <Modal
+    onClose={function noRefCheck(){}}
+  >
+    <Modal.Card>
+      <Modal.Card.Head>
+        <Modal.Card.Title>
+          Title
+        </Modal.Card.Title>
+      </Modal.Card.Head>
+      <Modal.Card.Body>
+            <Content>
+              <p>
+              </p>
+            </Content>
+      </Modal.Card.Body>
+      <Modal.Card.Foot
+        align="right"
+        hasAddons
+        renderAs={function noRefCheck(){}}
+      >
+        <Button color="success">
+          Like
+        </Button>
+        <Button>
+          Share
+        </Button>
+      </Modal.Card.Foot>
+    </Modal.Card>
+  </Modal>
+      <div className="navbar-end">
+        <Modal>
+        </Modal>
+      </div>
+
+<button className="js-modal-trigger navbar-item button is-primary" data-target="modal-js-example" onClick={toggleClass}>
+  Login/Sign Up
+</button>
+ */}
     </nav>
   )
 }
